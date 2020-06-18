cd9109bfa7412c951578e660ff481a80549cf0ef
youyou <1094465220@qq.com>
2020-06-18 19:05:18 +0800

    [liumengyao] - unfinished work with test should_return_west_when_given_left_command

---
+++ src/main/java/com/twschool/practice/MarsRover.java
@@ -0,0 +1,20 @@
+package com.twschool.practice;
+
+public class MarsRover {
+    int x;
+    int y;
+    String direction;
+    String commands;
+
+    public MarsRover(int x, int y, String direction, String commands) {
+        this.x = x;
+        this.y = y;
+        this.direction = direction;
+        this.commands = commands;
+    }
+    public String move(){
+        if("N".equals(direction)){
+            if()
+        }
+    }
+}
---
+++ src/test/java/com/twschool/practice/MarsRoverTest.java
@@ -0,0 +1,18 @@
+package com.twschool.practice;
+
+import org.junit.Test;
+
+public class MarsRoverTest {
+    @Test
+    public void should_return_west_when_given_left_command() {
+        //Given
+        MarsRover marsRover = new MarsRover(0,0,"N","L");
+
+        //When
+
+
+
+
+        //Then
+    }
+}
