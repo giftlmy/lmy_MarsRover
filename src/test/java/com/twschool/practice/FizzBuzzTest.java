46365214660aef8fa88883ec4d4b09e3894c5e4f
youyou <1094465220@qq.com>
2020-06-18 15:29:43 +0800

    [lmy] Init projecty

---
+++ src/test/java/com/twschool/practice/FizzBuzzTest.java
@@ -0,0 +1,52 @@
+package com.twschool.practice;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+import static org.hamcrest.CoreMatchers.*;
+import static org.hamcrest.MatcherAssert.assertThat;
+
+public class FizzBuzzTest {
+    @Test
+    public void should_return_1_given_1() {
+        FizzBuzzWhizz fizzBuzz = new FizzBuzzWhizz();
+        String result = fizzBuzz.say(1);
+        assertEquals("equals", "1", "1");
+    }
+
+    @Test
+    public void should_return_Fizz_given_3() {
+        FizzBuzzWhizz fizzBuzz = new FizzBuzzWhizz();
+        String result = fizzBuzz.say(3);
+        assertThat(result, is("Fizz"));
+    }
+
+    @Test
+    public void should_return_Buzz_given_5() {
+        FizzBuzzWhizz fizzBuzz = new FizzBuzzWhizz();
+        String result = fizzBuzz.say(5);
+        assertThat(result, is("Buzz"));
+    }
+
+    @Test
+    public void should_return_FizzBuzzWhizz_given_105() {
+        FizzBuzzWhizz fiz = new FizzBuzzWhizz();
+        String result = fiz.say(105);
+        assertThat(result,is("FizzBuzzWhizz"));
+    }
+
+    @Test
+    public void should_return_FizzWhizz_given_21() {
+        FizzBuzzWhizz fiz = new FizzBuzzWhizz();
+        String result = fiz.say(21);
+        assertThat(result,is("FizzWhizz"));
+    }
+
+    @Test
+    public void should_return_BuzzWhizz_given_35() {
+        FizzBuzzWhizz fiz = new FizzBuzzWhizz();
+        String result = fiz.say(35);
+        assertThat(result,is("BuzzWhizz"));
+
+    }
+}
