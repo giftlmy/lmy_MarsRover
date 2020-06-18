282f1c09fa97be6cea4f1df5e17f4e3d607ab42b
youyou <1094465220@qq.com>
2020-06-18 20:30:51 +0800

    [lmy] (0,0)EL

--- src/test/java/com/twschool/practice/MarsRoverTest.java
+++ src/test/java/com/twschool/practice/MarsRoverTest.java
@@ -114,6 +114,21 @@ public class MarsRoverTest {
         assertThat(marsRover.getCoordinate().getY(), is(0));
         assertThat(marsRover.getDirection(), is("E"));
     }
+    @Test
+    public void should_return_x0_y0_N_given_x0_y0_E_and_L() {
+        //given
+        Coordinate coordinate = new Coordinate(0, 0);
+        String direction = "E";
+
+        //when
+        MarsRover marsRover = new MarsRover(coordinate, direction);
+        marsRover.execute(Collections.singletonList("L"));
+
+        //then
+        assertThat(marsRover.getCoordinate().getX(), is(0));
+        assertThat(marsRover.getCoordinate().getY(), is(0));
+        assertThat(marsRover.getDirection(), is("N"));
+    }
 
 
 }
