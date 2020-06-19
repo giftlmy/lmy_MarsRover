b320f8dc84e89b8288b5c2025be507556a79e1f7
youyou <1094465220@qq.com>
2020-06-19 18:16:22 +0800

    [lmy] check mothods 0A4B

--- src/main/java/com/twschool/practice/GameAnswer.java
+++ src/main/java/com/twschool/practice/GameAnswer.java
@@ -1,10 +1,27 @@
 package com.twschool.practice;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 public class GameAnswer {
+    private final List<String> answerNumbers;
+
     public GameAnswer(String answerString) {
+        this.answerNumbers = Arrays.asList(answerString.split(" "));
     }
 
     public String check(String userAnswerString) {
-        return "4A0B";
+        List<String> userAnswerNumbers = Arrays.asList(userAnswerString.split(" "));
+        int valueAndPositionCorrectCount = 0;
+        int valueIncorrectAndPositionCorrectCount = 0;
+        for (int index = 0; index < answerNumbers.size(); index++) {
+            if (answerNumbers.get(index).equals(userAnswerNumbers.get(index))){
+                valueAndPositionCorrectCount ++;
+            }else if (answerNumbers.contains(userAnswerNumbers.get(index))){
+                valueIncorrectAndPositionCorrectCount ++;
+            }
+        }
+        return valueAndPositionCorrectCount + "A"+ valueIncorrectAndPositionCorrectCount +"B";
     }
 }
--- src/test/java/com/twschool/practice/GuessNumerTest.java
+++ src/test/java/com/twschool/practice/GuessNumerTest.java
@@ -5,15 +5,21 @@ import org.junit.Test;
 import static org.hamcrest.CoreMatchers.*;
 import static org.hamcrest.MatcherAssert.assertThat;
 public class GuessNumerTest {
-   @Test
-       public void should_return_4A0B_when_compare_with_1234_given_answer_1234() {
-           GameAnswer answer = new GameAnswer("1 2 3 4");
+    @Test
+    public void should_return_4A0B_when_compare_with_1234_given_answer_1234() {
+        GameAnswer answer = new GameAnswer("1 2 3 4");
 
-           String result = answer.check("1 2 3 4");
+        String result = answer.check("1 2 3 4");
 
-           Assert.assertEquals("4A0B",result);
-       }
+        Assert.assertEquals("4A0B", result);
+    }
 
+    @Test
+    public void should_return_0A4B_when_compare_with_1234_given_answer_4321() {
+        GameAnswer answer = new GameAnswer("1 2 3 4");
 
+        String result = answer.check("4 3 2 1");
 
+        Assert.assertEquals("0A4B", result);
+    }
 }
