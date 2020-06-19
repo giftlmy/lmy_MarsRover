853c0fbc93bcf1d3abce90fec86ee381e4512baa
youyou <1094465220@qq.com>
2020-06-19 18:33:21 +0800

    [lmy] 2A0b

--- src/test/java/com/twschool/practice/GuessNumerTest.java
+++ src/test/java/com/twschool/practice/GuessNumerTest.java
@@ -7,19 +7,31 @@ import static org.hamcrest.MatcherAssert.assertThat;
 public class GuessNumerTest {
     @Test
     public void should_return_4A0B_when_compare_with_1234_given_answer_1234() {
+        //given
         GameAnswer answer = new GameAnswer("1 2 3 4");
-
+        //when
         String result = answer.check("1 2 3 4");
-
+        //then
         Assert.assertEquals("4A0B", result);
     }
 
     @Test
-    public void should_return_0A4B_when_compare_with_1234_given_answer_4321() {
+    public void should_return_0A4B_when_compare_with_4321_given_answer_1234() {
+        //given
         GameAnswer answer = new GameAnswer("1 2 3 4");
-
+        //when
         String result = answer.check("4 3 2 1");
-
+        //then
         Assert.assertEquals("0A4B", result);
     }
+    @Test
+    public void should_return_2A0B_when_compare_with_1256_given_answer_1234() {
+        //given
+        GameAnswer answer = new GameAnswer("1 2 3 4");
+        //when
+        String result = answer.check("1 2 5 6");
+        //then
+        Assert.assertEquals("2A0B", result);
+    }
+
 }
