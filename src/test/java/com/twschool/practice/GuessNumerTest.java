395b95cddcc5fc86c23e721f8ae0524b3bebe158
youyou <1094465220@qq.com>
2020-06-19 17:07:50 +0800

    [lmy]4A0B

--- src/test/java/com/twschool/practice/GuessNumerTest.java
+++ src/test/java/com/twschool/practice/GuessNumerTest.java
@@ -1,16 +1,19 @@
 package com.twschool.practice;
 
+import org.junit.Assert;
 import org.junit.Test;
-
+import static org.hamcrest.CoreMatchers.*;
+import static org.hamcrest.MatcherAssert.assertThat;
 public class GuessNumerTest {
-    @Test
-    public void should_return_4A_0B_given_1_2_3_4() {
-        //given
-        String[] userAnswer = {"1","2","3","4"};
-        //when
-        GuessNumer guessNumer = new GuessNumer();
-        guessNumer.check(userAnswer);
-        //then
+   @Test
+       public void should_return_4A0B_when_compare_with_1234_given_answer_1234() {
+           GameAnswer answer = new GameAnswer("1 2 3 4");
 
-    }
+           String result = answer.check("1 2 3 4");
+
+           Assert.assertEquals("4A0B",result);
+       }
+
+
+
 }
